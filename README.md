565bf9eec83c2a53950171766618960c404c8768
Lete114 <me@imlete.cn>
2023-04-18 15:46:34 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,15 @@
+# My Tampermonkey Scripts
+
+一些我自己写的脚本，我用着舒服就行
+
+如果你有什么需求，可以发起 [issues](https://github.com/Lete114/my-tampermonkey-scripts/issues/new) 请求（哪天心情就写，或者付费代写脚本）
+
+## 目录
+
+| 网站        | 安装                                                                                          | 描述                                 |
+| ----------- | --------------------------------------------------------------------------------------------- | ------------------------------------ |
+| CSDN 文章页 | [安装](https://github.com/Lete114/my-tampermonkey-scripts/raw/main/csdn/blog/article.user.js) | 绿化 CSDN 文章页，提高可读性，易用性 |
+
+## LICENSE
+
+[MIT License](LICENSE)
---
+++ csdn/blog/article.user.js
@@ -0,0 +1,105 @@
+// ==UserScript==
+// @author       Lete114
+// @version      0.1
+// @name         CSDN 文章页绿化
+// @description  (自用脚本) 绿化 CSDN 文章页面、支持手机端 CSDN 文章页绿化
+// @match        https://blog.csdn.net/*/article/details/*
+// @icon         https://g.csdnimg.cn/static/logo/favicon32.ico
+// @namespace    https://github.com/Lete114/my-tampermonkey-scripts
+// @grant        GM_addStyle
+// ==/UserScript==
+
+;(function () {
+  'use strict'
+
+  const $ = (Selector) => document.querySelector(Selector)
+  const $$ = (Selector) => document.querySelectorAll(Selector)
+  const nullSpan = document.createElement('span')
+
+  GM_addStyle(`
+    /* 过滤文章页面的垃圾信息 */
+    #asideHotArticle,
+    #asideCategory,
+    #asideNewComments,
+    #asideNewNps,
+    #asideArchive,
+    #recommendNps,
+    #treeSkill,
+    .programmer1Box,
+    .hide-article-box,
+    .weixin-shadowbox,
+    span[data-href^='csdnapp://'],
+    #operate, .readall_box,
+    .btn_open_app_prompt_div { 
+        display: none !important
+    }
+    /* 解除文章内容可读限制 */
+    .article_content {
+        height: unset !important
+    }
+    /* 解除代码块无法自由选中 */
+    #content_views pre,
+    #content_views pre>code { 
+        -webkit-touch-callout: auto !important;
+        -webkit-user-select: auto !important;
+        -khtml-user-select: auto !important;
+        -moz-user-select: auto !important;
+        -ms-user-select: auto !important;
+        user-select: auto !important;
+    }
+    `)
+
+  // 优化文章侧边栏
+  const aside = $('.blog_container_aside')
+  if (aside) {
+    // 重排侧边栏顺序
+    aside.appendChild($('#asideSearchArticle') || nullSpan)
+    aside.appendChild($('#asideProfile') || nullSpan)
+    aside.appendChild($('#asidedirectory') || nullSpan)
+    aside.appendChild($('#asideArchive') || nullSpan)
+  }
+
+  // 关闭登录弹窗
+  const style = document.createElement('style')
+  style.textContent = '.passport-login-container{display:none}'
+  document.head.appendChild(style)
+  const timer = setInterval(() => {
+    const closeLogin = $('#passportbox>span')
+    if (closeLogin) {
+      clearInterval(timer)
+      closeLogin.click()
+      style.remove()
+    }
+  }, 500)
+
+  // 免登录复制
+  $$('pre>code').forEach((el) => {
+    el.setAttribute('onclick', 'mdcp.copyCode(event)')
+
+    const attrKey = 'data-title'
+    const attrValue = '免登录复制'
+    // 修改复制按钮文字
+    const observer = new MutationObserver((mutationList) => {
+      mutationList.forEach((mutation) => {
+        const target = mutation.target
+        if (
+          mutation.type === 'attributes' &&
+          target.getAttribute(attrKey) === '登录后复制'
+        )
+          target.setAttribute(attrKey, attrValue)
+      })
+    })
+    const btn = el.querySelector('.hljs-button')
+    btn.setAttribute(attrKey, attrValue)
+    observer.observe(btn, { attributes: true })
+  })
+
+  // 链接跳转(避免打开 link.csdn.net)
+  const content_views = $('#content_views')
+  const clone = content_views.cloneNode()
+  while (content_views.firstChild) clone.appendChild(content_views.firstChild)
+  content_views.parentNode.replaceChild(clone, content_views)
+  $$('#content_views a[href]').forEach((el) => {
+    el.setAttribute('target', '_blank')
+  })
+})()
